a206012c78617654e67cf8bae98fb3aae9010667
Ebikela <ebikela1@uab.edu>
2018-03-05 23:27:16 -0600

    my second webpage

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// alert("how are you");
+// var elements = document.getElementsByClassName("bannerdiv");
+//
+// // Declare a loop variable
+// var i;
+// // Four images side by side
+// function four() {
+//     for (i = 0; i < elements.length; i++) {
+//         elements[i].style.msFlex = "25%";  // IE10
+//         elements[i].style.flex = "25%";
+//     }
+// }
